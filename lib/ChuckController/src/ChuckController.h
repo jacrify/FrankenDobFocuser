623a50be7e6c62672e08d887bab224374d1e1927
John Cranney <john.cranney@gmail.com>
2023-08-31 08:33:38 +1000

    Moved initialisers to constructor

--- lib/ChuckController/src/ChuckController.h
+++ lib/ChuckController/src/ChuckController.h
@@ -1,8 +1,8 @@
 #ifndef CHUCK_CONTROLLER
 #define CHUCK_CONTROLLER
 
-#include "NunchukShared.h"
 #include "CurrentChukState.h"
+#include "NunchukShared.h"
 #define MODE_COUNT 5 // 0 up 1 right 2 down 3 left 4 limitfinder
 
 class ChuckController {
@@ -13,17 +13,24 @@ private:
   int minSpeeds[MODE_COUNT];
   int maxSpeeds[MODE_COUNT];
   int limitFlag;
-  int ledFlashCycle1 = 0;
-  int ledFlashCycle2 = 0;
+  int ledFlashCycle1;
+  int ledFlashCycle2;
   bool flashFast;
-  int memorySlot = -1;
-  bool memoryMoveFlag = 0;
-  bool memoryMoveInitiatatedFlag = 0;
-  
-  
-  int isLimitFindingMode = 0;
+  int memorySlot;
+  bool memoryMoveFlag;
+  bool memoryMoveInitiatatedFlag;
+
+  int isLimitFindingMode;
 
 public:
+  ChuckController() {
+    ledFlashCycle1 = 0;
+    ledFlashCycle2 = 0;
+    memorySlot=-1;
+     memoryMoveFlag = 0;
+     memoryMoveInitiatatedFlag=0;
+      isLimitFindingMode = 0;
+  }
   void processChuckData(wii_i2c_nunchuk_state state);
   int getSpeed();
   // int getDir();
@@ -31,12 +38,16 @@ public:
   void setMode(int mode);
   void setModeParameters(int mode, int minSpeedInHz, int maxSpeedInHz);
   bool getAndFlipLimitFlag();
-  bool isLimitFindingModeOn(); // when this mode is on we ignore limits when moving motor
-  int getLedsFlashCycle1(); //bitwise leds that should be lit on cycle1. 1=1, 2=2, 4=3, 8=4.
-  int getLedsFlashCycle2(); // bitwise leds that should be flashed. 1=1, 2=2, 4=3, 8=4.
-  bool getFlashFast(); //if true flash fast, otherwise flash slow
+  bool isLimitFindingModeOn(); // when this mode is on we ignore limits when
+                               // moving motor
+  int getLedsFlashCycle1(); // bitwise leds that should be lit on cycle1. 1=1,
+                            // 2=2, 4=3, 8=4.
+  int getLedsFlashCycle2(); // bitwise leds that should be flashed. 1=1, 2=2,
+                            // 4=3, 8=4.
+  bool getFlashFast();      // if true flash fast, otherwise flash slow
   bool getAndFlipMemoryMoveFlag(); // return whether a memory move is required
-  int getMemoryPosition();//returns the currently active memory slot. -1 means no slot selected.
+  int getMemoryPosition(); // returns the currently active memory slot. -1 means
+                           // no slot selected.
 private:
   CurrentChukState currentState;
 };

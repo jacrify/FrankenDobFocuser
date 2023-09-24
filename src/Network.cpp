c5fab97442f98c4b25df36a1fdf06303f3f7d68d
John Cranney <john.cranney@gmail.com>
2023-09-24 19:13:28 +1000

    Tested z cheat codes

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,23 +1,35 @@
 #include "Logging.h"
-#include <WiFiManager.h>
-WiFiManager wifiManager;
+
 #include <Preferences.h>
-// #include <WiFi.h>
+#include <WiFi.h>
 
-void loopNetwork(Preferences &prefs) {
-  if (digitalRead(0) == LOW) {
-    prefs.putBool("homeWifi", true);
+#define HOMEWIFISSID "HOMEWIFISSID"
+#define HOMEWIFIPASS "HOMEWIFIPASS"
 
-    delay(300);
-    esp_restart();
+void logIP() {
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    log("Waiting for connection...");
   }
+
+  // Once connected, log the IP address
+  IPAddress ip = WiFi.localIP();
+  log("Connected! IP address: %d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
 }
 
 void setupWifiHome(Preferences &prefs) {
-  log("Connecting to home wifi");
-  wifiManager.setConnectTimeout(10);
-  wifiManager.autoConnect();
-}
+  prefs.putString(HOMEWIFISSID,"***REMOVED***");
+  prefs.putString(HOMEWIFIPASS, "***REMOVED***");
+  if (prefs.isKey(HOMEWIFISSID) && prefs.isKey(HOMEWIFIPASS)) {
+    
+    log("Connecting to home wifi");
+    WiFi.begin(prefs.getString(HOMEWIFISSID), prefs.getString(HOMEWIFIPASS));
+    logIP();
+
+  } else {
+    log("Prefs not found :()");
+  }
+  }
 void setupWifi() {
 
   log("Creating access point");

27e0640d978274e349b709ea307fbc58a09c1c36
John Cranney <john.cranney@gmail.com>
2023-10-08 12:11:11 +1100

    added cstr to wifi

--- src/Network.cpp
+++ src/Network.cpp
@@ -23,7 +23,7 @@ void setupWifiHome(Preferences &prefs) {
   if (prefs.isKey(HOMEWIFISSID) && prefs.isKey(HOMEWIFIPASS)) {
     
     log("Connecting to home wifi");
-    WiFi.begin(prefs.getString(HOMEWIFISSID), prefs.getString(HOMEWIFIPASS));
+    WiFi.begin(prefs.getString(HOMEWIFISSID).c_str(), prefs.getString(HOMEWIFIPASS).c_str());
     logIP();
 
   } else {
